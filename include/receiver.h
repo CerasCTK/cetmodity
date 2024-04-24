802b5e92035ab989ed979df2c5cd3a48048611fc
CerasCTK <ceras410@gmail.com>
2024-04-24 10:30:11 +0700

    feat: add receiver struct

--- include/receiver.h
+++ include/receiver.h
@@ -1,4 +1,13 @@
 #ifndef CETMODITY_RECEIVER_H
 #define CETMODITY_RECEIVER_H
 
+#include "define.h"
+
+struct _receiver {
+    char name[HUMAN_MAX_NAME_LEN];
+    char phone_number[HUMAN_MAX_PHONE_LEN];
+    char address[HUMAN_MAX_ADDRESS_LEN];
+};
+typedef struct _receiver receiver;
+
 #endif // !CETMODITY_RECEIVER_H
