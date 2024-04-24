bcfc377bebe174a0c2f97dc7f23f00ca4d53a2e9
CerasCTK <ceras410@gmail.com>
2024-04-24 10:55:17 +0700

    feat: add sender struct

--- include/sender.h
+++ include/sender.h
@@ -1,4 +1,15 @@
 #ifndef CETMODITY_SENDER_H
 #define CETMODITY_SENDER_H
 
+#include "define.h"
+#include "dllist_item.h"
+
+struct _sender {
+    char name[HUMAN_MAX_NAME_LEN];
+    char phone_number[HUMAN_MAX_PHONE_LEN];
+    char address[HUMAN_MAX_ADDRESS_LEN];
+    dllist_item items;
+};
+typedef struct _sender sender;
+
 #endif // !CETMODITY_SENDER_H
