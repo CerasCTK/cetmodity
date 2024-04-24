c25f501a4921937b376515d3d8b735a6d43cf8bd
CerasCTK <ceras410@gmail.com>
2024-04-24 21:31:52 +0700

    feat: add declaration for deliver struct

--- include/deliver.h
+++ include/deliver.h
@@ -1,4 +1,14 @@
 #ifndef CETMODITY_DELIVER_H
 #define CETMODITY_DELIVER_H
 
+#include "define.h"
+#include "dllist_order.h"
+
+struct _deliver {
+    char name[HUMAN_MAX_NAME_LEN];
+    char phone_number[HUMAN_MAX_PHONE_LEN];
+    dllist_order orders;
+};
+typedef struct _deliver deliver;
+
 #endif // !CETMODITY_DELIVER_H
