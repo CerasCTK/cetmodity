a903a425f83ec520e7a04d5c95cb8ca4f39e0b75
CerasCTK <ceras410@gmail.com>
2024-04-24 11:34:24 +0700

    feat: add uuid function to generate uuid string

---
+++ include/util.h
@@ -0,0 +1,8 @@
+#ifndef CETMODITY_UTIL_H
+#define CETMODITY_UTIL_H
+
+#include <uuid/uuid.h>
+
+char *uuid(char[UUID_STR_LEN]);
+
+#endif // !CETMODITY_UTIL_H
---
+++ src/util.c
@@ -0,0 +1,9 @@
+#include "util.h"
+
+char *uuid(char out[UUID_STR_LEN]) {
+    uuid_t buffer;
+    uuid_generate(buffer);
+    uuid_unparse_upper(buffer, out);
+
+    return out;
+}
