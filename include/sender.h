5a4b2a41c91fe5410b798e4a432e021e8cf98220
CerasCTK <ceras410@gmail.com>
2024-04-24 11:01:44 +0700

    fix: remove dllist_item in sender struct

--- include/sender.h
+++ include/sender.h
@@ -2,13 +2,11 @@
 #define CETMODITY_SENDER_H
 
 #include "define.h"
-#include "dllist_item.h"
 
 struct _sender {
     char name[HUMAN_MAX_NAME_LEN];
     char phone_number[HUMAN_MAX_PHONE_LEN];
     char address[HUMAN_MAX_ADDRESS_LEN];
-    dllist_item items;
 };
 typedef struct _sender sender;
 
