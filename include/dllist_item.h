20b37dad89626a5c279d8adca39601d91b85fc17
CerasCTK <ceras410@gmail.com>
2024-04-24 20:11:46 +0700

    feat: provide declaration for some function in dllist_item

--- include/dllist_item.h
+++ include/dllist_item.h
@@ -1,8 +1,8 @@
 #ifndef CETMODITY_LLIST_ITEM_H
 #define CETMODITY_LLIST_ITEM_H
 
-#include "item.h"
 #include "bool.h"
+#include "item.h"
 
 struct _item_node {
     item item;
@@ -26,8 +26,24 @@ int di_size(dllist_item);
 
 void di_insert_begin(dllist_item *, item);
 
+void di_insert_end(dllist_item *, item);
+
 void di_insert_after(dllist_item *, item_node *, item);
 
 void di_insert_before(dllist_item *, item_node *, item);
+
+item_node *di_get_by_index(dllist_item, int);
+
+void di_delete(dllist_item *, item_node *);
+
+void di_delete_begin(dllist_item *);
+
+void di_delete_end(dllist_item *);
+
+void di_delete_before(dllist_item *, item_node *);
+
+void di_delete_after(dllist_item *, item_node *);
+
+void di_free(dllist_item *);
 
 #endif // !CETMODITY_LLIST_ITEM_H
