863eb1c4485e5d8202e5444afa2f00abb8860df8
CerasCTK <ceras410@gmail.com>
2024-04-24 14:26:07 +0700

    feat: add dllist_order struct

--- include/dllist_order.h
+++ include/dllist_order.h
@@ -1,4 +1,18 @@
 #ifndef CETMODITY_LLIST_ORDER_H
 #define CETMODITY_LLIST_ORDER_H
 
+#include "order.h"
+
+struct _order_node {
+    order order;
+    struct _order_node *next, *prev;
+};
+typedef struct _order_node order_node;
+
+struct _dllist_order {
+    order_node *head;
+    order_node *tail;
+};
+typedef struct _dllist_order dllist_order;
+
 #endif // !CETMODITY_LLIST_ORDER_H
