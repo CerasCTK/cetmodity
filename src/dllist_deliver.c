c92632fe67d443e0abef4e63a781c39c4a1d1bd1
Giapthe2002 <giapthuy2002@gmail.com>
2024-04-27 21:53:41 +0700

    feat: definition function in dllist_deliver.c

--- src/dllist_deliver.c
+++ src/dllist_deliver.c
@@ -1,1 +1,134 @@
 #include "dllist_deliver.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void dd_init(dllist_deliver *list) {
+    list->head = NULL;
+    list->tail = NULL;
+}
+
+bool dd_is_empty(dllist_deliver list) {
+    return list.head = NULL && list.tail == NULL;
+}
+
+bool dd_is_in_list(dllist_deliver list, deliver deliver) {
+    if (dd_is_empty(list)) return false;
+
+    while (list.head != NULL) {
+        if (is_same_deliver(list.head->deliver, deliver)) return true;
+        list.head = list.head->next;
+    }
+
+    return false;
+    
+}
+
+int dd_size(dllist_deliver list) {
+    if (dd_is_empty(list)) return 0;
+
+    int count = 0;
+    while(list.head != NULL) {
+        count++;
+        list.head = list.head->next;
+    }
+
+    return count;
+}
+
+void dd_insert_begin(dllist_deliver *list, deliver deliver) {
+    deliver_node *node = (deliver_node *)malloc(sizeof(deliver_node));
+    node->deliver = deliver;
+    node->prev = NULL;
+
+    if (dd_is_empty(*list)) {
+        list->tail = node;
+        node->next = NULL;
+    } else {
+        node->next = list->head;
+        list->head->prev = node;
+    }
+    list->head = node;
+}
+
+void dd_insert_end(dllist_deliver *list, deliver deliver) {
+    deliver_node *node = (deliver_node *)malloc(sizeof(deliver_node));
+    node->deliver = deliver;
+    node->next = NULL;
+
+    if (dd_is_empty(*list)) {
+        list->head = node;
+        node->prev = NULL;
+    } else {
+        node->prev = list->tail;
+        list->tail->next = node;
+    }
+    list->head = node;
+}
+
+void dd_insert_after(dllist_deliver *list, deliver_node *node, deliver deliver) {
+    if (dd_is_empty(*list)) return;
+    if (!(dd_is_in_list(*list, deliver)) || !(dd_is_in_list(*list, node->deliver))) return;
+
+    deliver_node *new_node = (deliver_node *)malloc(sizeof(deliver_node));
+    node->deliver = deliver;
+
+    if (list->tail == node) {
+        dd_insert_end(*list, deliver);
+    } else {
+        new_node->next = node->next;
+        node->next->prev = new_node;
+        node->next = new_node;
+        new_node->prev = node;
+    }
+}
+
+void dd_insert_before(dllist_deliver *list, deliver_node *node, deliver deliver) {
+    if (dd_is_empty(*list)) return;
+    if (!(dd_is_in_list(*list, deliver)) || !(dd_is_in_list(*list, node->deliver))) return;
+
+    deliver_node *new_node = (deliver_node *)malloc(sizeof(deliver_node));
+    node->deliver = deliver;
+
+    if (list->head == node) {
+        dd_insert_begin(*list, deliver);
+    } else {
+        new_node->prev = node->prev;
+        node->prev->next = new_node;
+        new_node->next = node;
+        node->prev = new_node;
+    }
+}
+
+deliver_node *dd_get_by_index(dllist_deliver list, int index) {
+    if (dd_is_empty(list)) return NULL;
+
+    if (index < 0 || index >= dd_size(list) - 1) return NULL;
+
+    if (index == 0) return list.head;
+    if (index == dd_size(list) - 1) return list.tail;
+
+    int count = 0;
+    item_node *node = list.head;
+    while (node != NULL) {
+        if (count == index) return list.head;
+        count++;
+        node = node->next;
+    }
+
+    return NULL;
+}
+
+item_node *di_search_node_by_name(dllist_item list, char* deliver_name) {
+    if (di_is_empty(list)) return NULL;
+
+    item_node *node = list.head;
+
+    while (node != NULL) {
+        if (strcmp(delvier_name, node->deliver.name)) return node;
+        node = node->next;
+    }
+
+    return NULL;
+}
