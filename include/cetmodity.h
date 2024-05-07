39eb6b52f57f6ea36a9f6e9312042b9493459104
CerasCTK <ceras410@gmail.com>
2024-05-07 16:06:13 +0700

    feat: add cetmodity core to run application

---
+++ include/cetmodity.h
@@ -0,0 +1,12 @@
+#ifndef CETMODITY_CETMODITY_H
+#define CETMODITY_CETMODITY_H
+
+void cetmodity_init();
+
+void cetmodity_run();
+
+void admin_menu();
+
+void deliver_menu();
+
+#endif // !CETMODITY_CETMODITY_H
---
+++ src/cetmodity.c
@@ -0,0 +1,27 @@
+#include "cetmodity.h"
+
+#include <sqlite3.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <uuid/uuid.h>
+
+#include "auth.h"
+#include "dllist_deliver.h"
+
+dllist_deliver delivers;
+
+void cetmodity_init() { dd_init(&delivers); }
+
+void cetmodity_run() {
+    char *found = login(delivers);
+
+    if (strcmp(found, "-ADMIN-") == 0) {
+        admin_menu();
+    } else {
+    }
+}
+
+void admin_menu() { printf("Admin menu"); }
+
+void deliver_menu() { printf("Deliver menu"); }
