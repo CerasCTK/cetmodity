ba0e2724a16dee43b73191fc8ec4d45caeb7ece6
CerasCTK <ceras410@gmail.com>
2024-05-07 15:59:28 +0700

    refactor(deliver): add id when create new deliver

--- src/deliver.c
+++ src/deliver.c
@@ -1,7 +1,9 @@
 #include "deliver.h"
-#include "dllist_order.h"
 
 #include <string.h>
+
+#include "dllist_order.h"
+#include "uuid_util.h"
 
 bool is_same_deliver(deliver first, deliver second) {
     return strcmp(first.phone_number, second.phone_number) == 0;
@@ -9,6 +11,7 @@ bool is_same_deliver(deliver first, deliver second) {
 
 deliver create_deliver(char *name, char *phone_number, account account) {
     deliver new_deliver = { .account = account };
+    uuid(new_deliver.id);
     strcpy(new_deliver.name, name);
     strcpy(new_deliver.phone_number, phone_number);
 
