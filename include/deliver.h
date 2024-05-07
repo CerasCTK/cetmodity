f92b5a7f2a4f2fdc8dbd4228ca587a32dd794228
CerasCTK <ceras410@gmail.com>
2024-05-07 15:06:17 +0700

    feat(deliver): add account to deliver struct

--- include/deliver.h
+++ include/deliver.h
@@ -1,12 +1,14 @@
 #ifndef CETMODITY_DELIVER_H
 #define CETMODITY_DELIVER_H
 
+#include "account.h"
 #include "define.h"
 #include "dllist_order.h"
 
 struct _deliver {
     char name[HUMAN_MAX_NAME_LEN];
     char phone_number[HUMAN_MAX_PHONE_LEN];
+    account account;
     dllist_order orders;
 };
 typedef struct _deliver deliver;
