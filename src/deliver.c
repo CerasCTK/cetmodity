b82d13e3555e3100ba0033cd0715ab4caf9d6b49
CerasCTK <ceras410@gmail.com>
2024-05-07 15:06:55 +0700

    refactor(deliver): compare deliver base on phone number

--- src/deliver.c
+++ src/deliver.c
@@ -3,5 +3,5 @@
 #include <string.h>
 
 bool is_same_deliver(deliver first, deliver second) {
-    return strcmp(first.name, second.name) == 0;
+    return strcmp(first.phone_number, second.phone_number) == 0;
 }
