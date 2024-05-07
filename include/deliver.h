140bcfd5f9c5cb0dbcdade88d429ea1e5a0a073e
CerasCTK <ceras410@gmail.com>
2024-05-07 15:56:49 +0700

    feat(deliver): add id for deliver struct

--- include/deliver.h
+++ include/deliver.h
@@ -1,11 +1,14 @@
 #ifndef CETMODITY_DELIVER_H
 #define CETMODITY_DELIVER_H
 
+#include <uuid/uuid.h>
+
 #include "account.h"
 #include "define.h"
 #include "dllist_order.h"
 
 struct _deliver {
+    char id[UUID_STR_LEN];
     char name[HUMAN_MAX_NAME_LEN];
     char phone_number[HUMAN_MAX_PHONE_LEN];
     account account;
