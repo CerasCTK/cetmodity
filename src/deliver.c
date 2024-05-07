060cdb8d6cf2488a677ebb8e4efe0b21610f24cc
CerasCTK <ceras410@gmail.com>
2024-05-07 16:00:51 +0700

    feat(deliver): compare deliver base on id

--- src/deliver.c
+++ src/deliver.c
@@ -6,7 +6,7 @@
 #include "uuid_util.h"
 
 bool is_same_deliver(deliver first, deliver second) {
-    return strcmp(first.phone_number, second.phone_number) == 0;
+    return strcmp(first.id, second.id) == 0;
 }
 
 deliver create_deliver(char *name, char *phone_number, account account) {
