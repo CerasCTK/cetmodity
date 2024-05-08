647974cf26b1f60c67481e86730a08274c87264f
CerasCTK <ceras410@gmail.com>
2024-05-08 10:03:06 +0700

    feat(cetmodity): add basic admin menu function

--- include/cetmodity.h
+++ include/cetmodity.h
@@ -1,12 +1,16 @@
 #ifndef CETMODITY_CETMODITY_H
 #define CETMODITY_CETMODITY_H
 
+#include "auth.h"
+
 void cetmodity_init();
 
 void cetmodity_run();
 
-void admin_menu();
+logout_state admin_menu();
 
 void deliver_menu();
+
+void show_admin_menu();
 
 #endif // !CETMODITY_CETMODITY_H
--- src/cetmodity.c
+++ src/cetmodity.c
@@ -1,11 +1,11 @@
 #include "cetmodity.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <uuid/uuid.h>
 
-#include "auth.h"
 #include "dllist_deliver.h"
 
 dllist_deliver delivers;
@@ -13,14 +13,43 @@ dllist_deliver delivers;
 void cetmodity_init() { dd_init(&delivers); }
 
 void cetmodity_run() {
-    char *found = login(delivers);
+    char *found;
+    logout_state state;
+RELOGIN:
+    found = login(delivers);
 
-    if (strcmp(found, "-ADMIN-") == 0) {
-        admin_menu();
-    } else {
+    if (strcmp(found, "-ADMIN-") == 0)
+        state = admin_menu();
+    else
+        deliver_menu();
+
+    if (state == LOGOUT_STATE)
+        goto RELOGIN;
+}
+
+logout_state admin_menu() {
+    int opt;
+    while (true) {
+        system("clear");
+        show_admin_menu();
+        printf("Input your option: ");
+        scanf("%d", &opt);
+        switch (opt) {
+            case 1: printf("delivers\n"); break;
+            case 2: printf("orders\n"); break;
+            case 0: return logout();
+            default:
+                printf("Invalid choice, use only the options above\n");
+                break;
+        }
     }
 }
 
-void admin_menu() { printf("Admin menu"); }
-
 void deliver_menu() { printf("Deliver menu"); }
+
+void show_admin_menu() {
+    printf("ADMIN MENU\n");
+    printf("1. Manage delivers\n");
+    printf("2. Manage orders\n");
+    printf("0. Log out\n");
+}
