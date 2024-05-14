ba1da08bf6bfa928382c5970714883b91ddd1fde
CerasCTK <ceras410@gmail.com>
2024-05-14 15:24:35 +0700

    feat: add tax to define.h

--- include/define.h
+++ include/define.h
@@ -1,9 +1,17 @@
 #ifndef CETMODITY_DEFINE_H
 #define CETMODITY_DEFINE_H
 
+#if 1 // For human struct (sender, receiver, deliver)
 #define HUMAN_MAX_NAME_LEN 20
 #define HUMAN_MAX_PHONE_LEN 10
+#endif // end
 
+#if 1 // For item
 #define PRODUCT_MAX_NAME_LEN 40
+#endif // end
+
+#if 1 // For order
+#define SHIP_TAX 0.1
+#endif // end
 
 #endif // !CETMODITY_DEFINE_H
