96505da186f8d6856561f58aa67de07cb105981a
CerasCTK <ceras410@gmail.com>
2024-05-19 20:16:30 +0700

    refactor(dllist_item): add param name for all functions in header file

--- include/dllist_item.h
+++ include/dllist_item.h
@@ -16,38 +16,38 @@ struct _dllist_item {
 };
 typedef struct _dllist_item dllist_item;
 
-void di_init(dllist_item *);
+void di_init(dllist_item *list);
 
-bool di_is_empty(dllist_item);
+bool di_is_empty(dllist_item list);
 
-bool di_is_in_list(dllist_item, item);
+bool di_is_in_list(dllist_item list, item item);
 
-int di_size(dllist_item);
+int di_size(dllist_item list);
 
-void di_insert_begin(dllist_item *, item);
+void di_insert_begin(dllist_item *list, item item);
 
-void di_insert_end(dllist_item *, item);
+void di_insert_end(dllist_item *list, item item);
 
-void di_insert_after(dllist_item *, item_node *, item);
+void di_insert_after(dllist_item *list, item_node *node, item item);
 
-void di_insert_before(dllist_item *, item_node *, item);
+void di_insert_before(dllist_item *list, item_node *node, item item);
 
-item_node *di_get_by_index(dllist_item, int);
+item_node *di_get_by_index(dllist_item list, int index);
 
-item_node *di_search_node_by_name(dllist_item, char *);
+item_node *di_search_node_by_name(dllist_item list, char *item_name);
 
-void di_delete(dllist_item *, item_node *);
+void di_delete(dllist_item *list, item_node *node);
 
-void di_delete_begin(dllist_item *);
+void di_delete_begin(dllist_item *list);
 
-void di_delete_end(dllist_item *);
+void di_delete_end(dllist_item *list);
 
-void di_delete_before(dllist_item *, item_node *);
+void di_delete_before(dllist_item *list, item_node *node);
 
-void di_delete_after(dllist_item *, item_node *);
+void di_delete_after(dllist_item *list, item_node *node);
 
-void di_free(dllist_item *);
+unsigned long di_calculate_total_price(dllist_item list);
 
-unsigned long di_calculate_total_price(dllist_item);
+void di_free(dllist_item *list);
 
 #endif // !CETMODITY_LLIST_ITEM_H
