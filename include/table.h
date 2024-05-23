998a56aca7858abdb8d7957f4dc30b1fc3de681a
CerasCTK <ceras410@gmail.com>
2024-05-24 01:14:00 +0700

    style: reformat table.{h, c}

--- include/table.h
+++ include/table.h
@@ -2,9 +2,10 @@
 #define CETMODITY_TABLE_H
 
 #include "dllist_order.h"
-#include "string_converter.h"
 
-#define TABLE_BORDER_X "-----------------------------------------------------------------------------------------------------------"
+#define TABLE_BORDER_X                                                         \
+    "------------------------------------------------------------------------" \
+    "-----------------------------------"
 
 void show_order_table(dllist_order list_order);
 
--- src/table.c
+++ src/table.c
@@ -3,14 +3,19 @@
 #include <stdio.h>
 #include <uuid/uuid.h>
 
-#define PRINT_BORDER_X(border_len) (printf("%.*s\n", border_len, TABLE_BORDER_X))
+#include "string_converter.h"
+
+#define PRINT_BORDER_X(border_len)                                             \
+    (printf("%.*s\n", border_len, TABLE_BORDER_X))
 
 void show_order_table(const dllist_order list_order) {
     // | INDEX | ID | SENDER | RECEIVER | LIST ITEM |
     const int QUANTITY_ITEMS = 5;
     const int NUM_OF_SEPARATE_CHAR = 6;
     const int INDEX_LEN = 3;
-    const int border_x = INDEX_LEN + UUID_STR_LEN + SENDER_MAX_NAME_LEN + RECEIVER_MAX_NAME_LEN + PRODUCT_MAX_NAME_LEN + NUM_OF_SEPARATE_CHAR + QUANTITY_ITEMS;
+    const int border_x = INDEX_LEN + UUID_STR_LEN + SENDER_MAX_NAME_LEN
+                         + RECEIVER_MAX_NAME_LEN + PRODUCT_MAX_NAME_LEN
+                         + NUM_OF_SEPARATE_CHAR + QUANTITY_ITEMS;
 
     PRINT_BORDER_X(border_x);
     printf("|%*s|", INDEX_LEN, "IND");
@@ -23,7 +28,8 @@ void show_order_table(const dllist_order list_order) {
         PRINT_BORDER_X(border_x);
         const order_node *n_order = do_get_by_index(list_order, i);
 
-        if (n_order == NULL) continue;
+        if (n_order == NULL)
+            continue;
 
         order order = n_order->order;
         const dllist_item item_list = order.items;
@@ -35,19 +41,23 @@ void show_order_table(const dllist_order list_order) {
         printf("%*s|", UUID_STR_LEN, order.id);
         printf("%*s|", SENDER_MAX_NAME_LEN, order.sender.name);
         printf("%*s|", RECEIVER_MAX_NAME_LEN, order.receiver.name);
-        printf("%*s|\n", PRODUCT_MAX_NAME_LEN + QUANTITY_ITEMS, item_info_array[0]);
+        printf(
+            "%*s|\n", PRODUCT_MAX_NAME_LEN + QUANTITY_ITEMS, item_info_array[0]
+        );
 
         for (int j = 1; j < item_list_size; j++) {
             printf("|%*s|", INDEX_LEN, "");
             printf("%*s|", UUID_STR_LEN, "");
             printf("%*s|", SENDER_MAX_NAME_LEN, "");
             printf("%*s|", RECEIVER_MAX_NAME_LEN, "");
-            printf("%*s|\n", PRODUCT_MAX_NAME_LEN + QUANTITY_ITEMS, item_info_array[j]);
+            printf(
+                "%*s|\n", PRODUCT_MAX_NAME_LEN + QUANTITY_ITEMS,
+                item_info_array[j]
+            );
         }
 
         free_list_item_strings(item_info_array, item_list_size);
     }
 
     PRINT_BORDER_X(border_x);
-
 }
