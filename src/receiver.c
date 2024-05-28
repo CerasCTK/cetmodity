53759f9b51efaca61e5aad2fe4f9972b07cb755f
Giapthe2002 <giapthuy2002@gmail.com>
2024-05-28 15:53:06 +0700

    fix: input using scanf %s can not contain space char

--- src/receiver.c
+++ src/receiver.c
@@ -1,6 +1,8 @@
 #include "receiver.h"
+
 #include <stdio.h>
 #include <string.h>
+#include "io_util.h"
 
 receiver create_receiver(
     const char *name, const char *phone_number, const coordinate location
@@ -13,17 +15,17 @@ receiver create_receiver(
 }
 
 receiver create_receiver_input() {
-    receiver new_receiver;
+    char name[RECEIVER_MAX_NAME_LEN];
+    char phone_number[RECEIVER_MAX_PHONE_LEN];
 
     printf("Input the receiver's name: ");
-    scanf("%s", new_receiver.name);
+    input_string(name, RECEIVER_MAX_NAME_LEN);
     printf("Input the receiver's phone number: ");
-    scanf("%s", new_receiver.phone_number);
-    printf("Input the receiver location: ");
-    scanf(
-        "%lf %lf", &new_receiver.location.latitude,
-        &new_receiver.location.longitude
-    );
+    input_string(phone_number, RECEIVER_MAX_PHONE_LEN);
 
-    return new_receiver;
+    double latitude, longitude;
+    printf("Input the receiver location: ");
+    scanf("%lf %lf", &latitude, &longitude);
+
+    return create_receiver(name, phone_number, { .latitude = latitude, .longitude = longitude });
 }
