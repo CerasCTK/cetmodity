fd113f3b6b83fd8dbe09a2ee289b96e1ff7d5868
CerasCTK <ceras410@gmail.com>
2024-06-05 14:36:19 +0700

    docs(cetmodity): add docs for cetmodity file

--- include/cetmodity.h
+++ include/cetmodity.h
@@ -3,16 +3,33 @@
 
 #include "auth.h"
 
+/**
+ * @brief Initial cetmodity application
+ */
 void cetmodity_init();
 
+/**
+ * @brief Run loop for cetmodity application
+ */
 void cetmodity_run();
 
+/**
+ * @brief Menu for admin
+ *
+ * @return State of logout
+ */
 logout_state admin_menu();
 
 void deliver_menu();
 
+/**
+ * @brief Menu for admin to manage delivers
+ */
 void admin_manage_delivers();
 
+/**
+ * @brief Menu for admin to manage orders
+ */
 void admin_manage_orders();
 
 #endif // !CETMODITY_CETMODITY_H
