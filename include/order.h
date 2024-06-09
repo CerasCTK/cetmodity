b52868c2fa3f804e54da0b197c073196a6cabca7
CerasCTK <ceras410@gmail.com>
2024-06-09 15:49:14 +0700

    refactor&docs(order): using pointer and add docs

--- include/order.h
+++ include/order.h
@@ -10,27 +10,72 @@
 #define SHIP_TAX 0.05
 #define ORDER_STATUS_LENGTH 31
 
+/**
+ * @typedef order_status
+ * @brief Status of the order.
+ *
+ */
 typedef enum order_status { in_storage, in_transit, delivered } order_status;
 
+/**
+ * @typedef order
+ * @brief Struct of order.
+ *
+ */
 typedef struct order {
     char id[ID_LEN];
+
     char deliver_id[ID_LEN];
+
     sender sender;
     receiver receiver;
-    dllist_item items;
+
+    dllist_item *item_list;
+
     unsigned long items_price;
     unsigned int shipping_fee;
+
     order_status status;
 } order;
 
-bool is_same_order(order o1, order o2);
+/**
+ * @brief Compare two orders.
+ *
+ * @param o1 First order to compare.
+ * @param o2 Second order to compare.
+ * @return true if the two orders has the same id, false otherwise.
+ */
+const bool is_same_order(const order *const o1, const order *const o2);
 
-order create_empty_order(sender sender, receiver receiver);
+/**
+ * @brief Create new order with zero items.
+ *
+ * @param sender Sender of the order.
+ * @param receiver Receiver of the order.
+ * @return New order with the given information.
+ */
+order *const create_empty_order(const sender sender, const receiver receiver);
 
-void order_add_item(order *order, item new_item);
+/**
+ * @brief Add item to the order.
+ *
+ * @param order Order to add the item.
+ * @param new_item New item to add.
+ */
+void order_add_item(order *const order, item *const new_item);
 
-void order_add_item_input(order *order);
+/**
+ * @brief Add item to the order with input.
+ *
+ * @param order Order to add the item.
+ */
+void order_add_item_input(order *const order);
 
-void show_order_information(order order);
+/**
+ * @brief Show the information of the order.
+ *
+ * @param order Order to show the information.
+ */
+void show_order_information(const order *const order);
 
 #endif // !CETMODITY_ORDER_H
--- src/order.c
+++ src/order.c
@@ -4,47 +4,54 @@
 #include "uuid_util.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <table.h>
 
-bool is_same_order(const order o1, const order o2) {
-    return strcmp(o1.id, o2.id) == 0;
+const bool is_same_order(const order *const o1, const order *const o2) {
+    return strcmp(o1->id, o2->id) == 0;
 }
 
-order create_empty_order(const sender sender, const receiver receiver) {
-    order new_order = { .sender = sender,
-                        .deliver_id = "",
-                        .receiver = receiver,
-                        .items_price = 0,
-                        .shipping_fee = 0,
-                        .status = in_storage };
+order *const create_empty_order(const sender sender, const receiver receiver) {
+    order *new_order = malloc(sizeof(order));
 
-    uuid(new_order.id);
+    uuid(new_order->id);
 
-    di_init(&new_order.items);
+    strcpy(new_order->deliver_id, "");
+
+    new_order->sender = sender;
+    new_order->receiver = receiver;
+
+    di_init(new_order->item_list);
+
+    new_order->items_price = 0;
+    new_order->shipping_fee = 0;
+
+    new_order->status = in_storage;
 
     return new_order;
 }
 
-void order_add_item(order *order, const item new_item) {
-    di_insert_end(&order->items, new_item);
+void order_add_item(order *const order, item *const new_item) {
+    di_insert(order->item_list, new_item);
 
-    order->items_price = di_calculate_total_price(order->items);
+    // Update price
+    order->items_price = di_calculate_total_price(order->item_list);
     order->shipping_fee = order->items_price * SHIP_TAX;
 }
 
-void order_add_item_input(order *order) {
-    item new_item = create_item_input();
+void order_add_item_input(order *const order) {
+    item *new_item = create_item_input();
 
     order_add_item(order, new_item);
 }
 
-void show_order_information(order order) {
+void show_order_information(const order *const order) {
     char status[ORDER_STATUS_LENGTH];
 
-    if (order.status == 0) {
+    if (order->status == 0) {
         strcpy(status, "In storage");
-    } else if (order.status == 1) {
+    } else if (order->status == 1) {
         strcpy(status, "In transit");
     } else {
         strcpy(status, "Delived");
