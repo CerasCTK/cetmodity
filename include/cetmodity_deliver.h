f8387c3e805bb516d838f718a044b3f66d2aea5e
CerasCTK <ceras410@gmail.com>
2024-06-09 19:12:33 +0700

    feat(cetmodity_deliver): using pointer so that we only need to change order in deliver

--- include/cetmodity_deliver.h
+++ include/cetmodity_deliver.h
@@ -1,9 +1,8 @@
 #ifndef CETMODITY_CETMODITY_DELIVER_H
 #define CETMODITY_CETMODITY_DELIVER_H
 
-#include "dllist_deliver.h"
-#include "dllist_order.h"
+#include "deliver.h"
 
-void complete_order(dllist_order *order_list, dllist_deliver *deliver_list);
+void complete_order(deliver *const deliver);
 
 #endif
--- src/cetmodity_deliver.c
+++ src/cetmodity_deliver.c
@@ -1,27 +1,15 @@
 #include "cetmodity_deliver.h"
-#include "cetmodity_menu.h"
 
 #include <stdio.h>
 
-void complete_order(dllist_order *order_list, dllist_deliver *deliver_list) {
-    order_node *node = do_search_by_id_input(*order_list);
+void complete_order(deliver *const deliver) {
+    order_node *node = do_search_by_id_input(deliver->orders);
     if (node == NULL) {
         printf("No order found with that ID!\n");
-    }
-
-    dllist_order order_list_of_deliver
-        = dd_search_node_by_id(*deliver_list, node->order.deliver_id)
-              ->deliver.orders;
-    order_node *order_of_deliver
-        = do_search_by_id(order_list_of_deliver, node->order.id);
-
-    if (order_of_deliver->order.status == delivered) {
-        printf("Order has been delivered!\n");
         return;
     }
 
-    node->order.status = delivered;
-    order_of_deliver->order.status = delivered;
+    node->order->status = delivered;
 
     printf("Completed order successfully!\n");
 }
