246fbd1c25606736bc0c8d7e3ac7578adcc1a68b
CerasCTK <ceras410@gmail.com>
2024-06-17 01:53:36 +0700

    feat: always save data to database

--- src/database.c
+++ src/database.c
@@ -31,13 +31,9 @@ void load_database(dllist_deliver *const delivers, dllist_order *const orders) {
 void save_data_to_database(
     dllist_deliver *const delivers, dllist_order *const orders
 ) {
-    if (dd_size(delivers) != 0) {
-        write_delivers_to_file(DB_DELIVER_FILE, delivers);
-    }
+    write_delivers_to_file(DB_DELIVER_FILE, delivers);
 
-    if (do_size(orders) != 0) {
-        write_orders_to_file(DB_ORDER_FILE, orders);
-    }
+    write_orders_to_file(DB_ORDER_FILE, orders);
 
     write_linker_to_file(DB_LINKER_FILE, orders);
 }
